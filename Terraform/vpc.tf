ac98d6d3669e3b8374d378312562af0999ca2368
carmel-okun <carmel.okun@nitzanim.tech>
2024-10-04 15:56:45 +0300

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -101,6 +101,11 @@ resource "aws_route_table_association" "private_a1" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_route_table_association" "private_a2" {
+  subnet_id      = aws_subnet.private_a2.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_route_table_association" "private_b" {
   subnet_id      = aws_subnet.private_b.id
   route_table_id = aws_route_table.private.id
