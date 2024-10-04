ba5fc6696229ea78c8fa66ed96fc50b8b4e059c9
carmel-okun <carmel.okun@nitzanim.tech>
2024-10-04 17:02:29 +0300

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -13,16 +13,16 @@ resource "aws_vpc" "main" {
 
 # Create Public Subnets
 resource "aws_subnet" "public_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
@@ -65,7 +65,7 @@ resource "aws_nat_gateway" "nat" {
     Name = "gw NAT"
   }
 
-  depends_on = [aws_internet_gateway.main]  # Ensure the Internet Gateway is created first
+  depends_on = [aws_internet_gateway.main] # Ensure the Internet Gateway is created first
 }
 
 # Route Table for Public Subnets
