55727a51f277bc3584f9c309bf41d6da5517c954
Yoram Izilov <66864506+Yoram-Izilov@users.noreply.github.com>
2024-10-05 17:54:30 +0300

    Update bastion.tf

--- Terraform/bastion.tf
+++ Terraform/bastion.tf
@@ -1,3 +1,26 @@
+# Attach IAM Role to Bastion Instance for Session Manager
+resource "aws_iam_role" "ssm_role" {
+  name               = "BastionSSMRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Action": "sts:AssumeRole",
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "ec2.amazonaws.com"
+    }
+  }
+}
+EOF
+}
+
+# Attach SSM policies
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_instance" "bastion" {
   ami           = "ami-0866a3c8686eaeeba"  # Ubuntu linux t2.micro
   instance_type = "t2.micro"
@@ -9,6 +32,11 @@ resource "aws_instance" "bastion" {
 
   # Security group for SSH access
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "BastionSSMInstanceProfile"
+  role = aws_iam_role.ssm_role.name
 }
 
 # Security Group for the Bastion Host
