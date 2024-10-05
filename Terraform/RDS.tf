ad4f2d16e02a2edba75927dc7ee1c96167561113
Yoram Izilov <66864506+Yoram-Izilov@users.noreply.github.com>
2024-10-05 11:09:34 +0300

    Update RDS.tf

--- Terraform/RDS.tf
+++ Terraform/RDS.tf
@@ -9,11 +9,40 @@ resource "aws_db_instance" "postgres" {
   engine               = "postgres"
   engine_version       = "14.9"
   instance_class       = "db.t3.micro"
-  dbname               = "yoram-carmel-database"
+  dbname               = "yoram_carmel_db"
   username             = "admin"
   password             = "password"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name = aws_db_subnet_group.db_subnet.id
   publicly_accessible  = false
   skip_final_snapshot  = true
+}
+
+# PostgreSQL Provider Configuration
+provider "postgresql" {
+  host     = aws_db_instance.postgres.address
+  port     = aws_db_instance.postgres.port
+  database = "postgres"  # Connect to the default database to create the user and assign ownership
+  username = "admin"
+  password = "password"
+}
+
+# Creates status-page user and log into it
+resource "postgresql_role" "status_page_user" {
+  name     = "status-page"
+  password = "status-page"
+  login    = true
+}
+
+# Grant ownership of the database to the status-page user
+resource "postgresql_database" "yoram_carmel_db" {
+  name  = aws_db_instance.postgres.dbname
+  owner = postgresql_role.status_page_user.name
+}
+
+# Grant privileges
+resource "postgresql_grant" "status_page_db_grant" {
+  database    = postgresql_database.database.name
+  role        = postgresql_role.status_page_user.name
+  privileges  = ["ALL"]
 }
