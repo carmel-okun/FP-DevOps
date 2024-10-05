cad193963bef62ba5011bbce535ea982fee729e5
Yoram Izilov <66864506+Yoram-Izilov@users.noreply.github.com>
2024-10-05 16:52:31 +0300

    Update ElastiCache.tf

--- Terraform/ElastiCache.tf
+++ Terraform/ElastiCache.tf
@@ -7,9 +7,9 @@ resource "aws_elasticache_subnet_group" "redis_subnet" {
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-cluster"
   engine               = "redis"
-  node_type            = "cache.t3.micro"
+  node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
+  parameter_group_name = "default.redis7"
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet.name
   security_group_ids   = [aws_security_group.redis_sg.id]
 }
