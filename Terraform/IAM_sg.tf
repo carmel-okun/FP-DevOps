53a86a8bcbe7acdaece8943f29fe4d07576110cd
carmel-okun <carmel.okun@nitzanim.tech>
2024-10-05 22:01:22 +0300

    RDS work

--- Terraform/IAM_sg.tf
+++ Terraform/IAM_sg.tf
@@ -34,7 +34,6 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # maybe delete
     security_groups = [aws_security_group.lb_sg.id]
   }
 
@@ -55,7 +54,6 @@ resource "aws_security_group" "redis_sg" {
     from_port       = 6379
     to_port         = 6379
     protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # maybe delete
     security_groups = [aws_security_group.lb_sg.id]
   }
 
--- Terraform/RDS.tf
+++ Terraform/RDS.tf
@@ -10,7 +10,7 @@ terraform {
 # PostgreSQL RDS
 resource "aws_db_subnet_group" "db_subnet" {
   name       = "rds-subnet-group"
-  subnet_ids = [aws_subnet.private_a1.id, aws_subnet.private_a2.id, aws_subnet.private_b.id, aws_subnet.public_a.id]
+  subnet_ids = [aws_subnet.private_a1.id, aws_subnet.private_a2.id, aws_subnet.private_b.id]
 }
 
 resource "aws_db_instance" "postgres" {
@@ -18,41 +18,22 @@ resource "aws_db_instance" "postgres" {
   engine                 = "postgres"
   engine_version         = "16.3"
   instance_class         = "db.t3.micro"
-  db_name                = "yoram_carmel_db"
-  username               = "postgres_admin"
+  db_name                = "status_page"
+  username               = "status_page"
   password               = "password"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.db_subnet.id
   publicly_accessible    = false
   skip_final_snapshot    = true
+  storage_type           = "gp3"
 }
 
 # PostgreSQL Provider Configuration
-provider "postgresql" {
-  host     = aws_db_instance.postgres.address
-  port     = aws_db_instance.postgres.port
-  database = "postgres" # Connect to the default database to create the user and assign ownership
-  username = "postgres_admin"
-  password = "password"
-}
-
-# Creates status-page user and log into it
-resource "postgresql_role" "status_page_user" {
-  name     = "status-page"
-  password = "status-page"
-  login    = true
-}
-
-# Grant ownership of the database to the status-page user
-resource "postgresql_database" "yoram_carmel_db" {
-  name  = aws_db_instance.postgres.db_name
-  owner = postgresql_role.status_page_user.name
-}
-
-# Grant privileges
-resource "postgresql_grant" "status_page_db_grant" {
-  database    = postgresql_database.yoram_carmel_db.name
-  role        = postgresql_role.status_page_user.name
-  privileges  = ["ALL"]
-  object_type = "database"
-}
+# provider "postgresql" {
+#   host     = aws_db_instance.postgres.address
+#   port     = aws_db_instance.postgres.port
+#   database = aws_db_instance.postgres.db_name
+#   username = aws_db_instance.postgres.username
+#   password = aws_db_instance.postgres.password
+#   sslmode  = "require"
+# }
