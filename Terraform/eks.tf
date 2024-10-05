ba4d4d072ec2ba86afaa9e4f83a90b31d6e4dc36
Yoram Izilov <66864506+Yoram-Izilov@users.noreply.github.com>
2024-10-05 16:54:22 +0300

    Update eks.tf

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -40,7 +40,7 @@ module "eks" {
 
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = module.eks.cluster_name
-  node_group_name = "eks-node-group"
+  node_group_name = "app-eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [aws_subnet.private_a1.id, aws_subnet.private_b.id]
   scaling_config {
