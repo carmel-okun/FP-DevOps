60b0ebee12e7292e3d6aee8b66a3ef030e5972ff
Yoram Izilov <66864506+Yoram-Izilov@users.noreply.github.com>
2024-10-05 12:43:46 +0300

    Update ALB_WAF.tf

--- Terraform/ALB_WAF.tf
+++ Terraform/ALB_WAF.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "app_lb" {
 
 resource "aws_waf_web_acl" "waf" {
   name        = "yoram-carmel-waf"
-  metric_name = "yoram_carmel_waf_metric"
+  metric_name = "YoramCarmelWafMetric"
   default_action {
     type = "ALLOW"
   }
