6f601d8aabdb0d9c546bded31f0ba53bab025d64
Yoram Izilov <66864506+Yoram-Izilov@users.noreply.github.com>
2024-10-05 10:42:40 +0300

    changed cluster and iam role name

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -1,6 +1,6 @@
 # IAM Role for EKS
 resource "aws_iam_role" "eks_role" {
-  name = "eks_role"
+  name = "carmel_yoram_eks_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -17,7 +17,7 @@ resource "aws_iam_role" "eks_role" {
 # EKS Cluster
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "yoram-carmel-eks-cluster"
+  cluster_name    = "carmel-yoram-eks-cluster"
   cluster_version = "1.21"
   subnets         = [aws_subnet.private_a1.id, aws_subnet.private_a2.id,  aws_subnet.private_b.id]
   vpc_id          = aws_vpc.main.id
