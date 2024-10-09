c87846b17989825d7b002b4793b96b1ef38abbb5
carmel-okun <carmel.okun@nitzanim.tech>
2024-10-09 19:06:19 +0300

    updating to remove EKS related TF files

--- Terraform/2-ALB_WAF.tf
+++ Terraform/2-ALB_WAF.tf
@@ -1,11 +1,11 @@
 # Application Load Balancer
-resource "aws_lb" "app_lb" {
-  name               = "yoram-carmel-app-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-}
+# resource "aws_lb" "app_lb" {
+#   name               = "yoram-carmel-app-lb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.lb_sg.id]
+#   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+# }
 
 # resource "aws_waf_web_acl" "waf" {
 #   name        = "yoram-carmel-waf"
--- Terraform/3-IAM_sg.tf
+++ Terraform/3-IAM_sg.tf
@@ -1,29 +1,29 @@
 # Security Group for ALB
-resource "aws_security_group" "lb_sg" {
-  vpc_id = aws_vpc.main.id
-  name   = "yoram_carmel_lb_sg"
+# resource "aws_security_group" "lb_sg" {
+#   vpc_id = aws_vpc.main.id
+#   name   = "yoram_carmel_lb_sg"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 # Security Group for RDS
 resource "aws_security_group" "rds_sg" {
